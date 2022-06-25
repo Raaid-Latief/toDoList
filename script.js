46e059071435543aed303d3a1b3e24a5976bb376
Raaid-Latief <raaidlatiefgbr@gmail.com>
2022-06-25 12:59:50 +0200

    5th commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const text = document.getElementById("add");
+const sortButton= document.getElementById("sort-btn");
 const addTaskButton = document.getElementById("add-task-btn");
 const saveTaskButton = document.getElementById("save-todo-btn");
 const listBox = document.getElementById("listBox");
@@ -29,23 +30,23 @@ addTaskButton.addEventListener("click", (e) => {
 
 function displayTodo() {
  let todo = localStorage.getItem("todo");
- if (todo === null) {
-   todoArray = [];
- } else {
+ {
    todoArray = JSON.parse(todo);
  }
  let htmlCode = "";
  todoArray.forEach((list, ind) => {
-   htmlCode += `<div class='d-flex flex-row p-2'>
- 
-   
-   <p id="displayedItems" class='text-success'> - ${list}</p>
+   htmlCode += `<div class='d-flex flex-row p-1'>
+   <li class="d-flex flex-row gap-2" id="${ind}">
+  <input type="checkbox" onclick="itemCompleted(${ind}) checked" class="mt-4 ms-2 chkItem form-check-input">
+   <p id="displayedItems" class='text-success text-center'> - ${list}</p>
   <button onclick='deleteTodo(${ind})'  <i id="close-btn" class="btn btn-outline-danger fa-solid fa-circle-xmark"></i></button>
 
 </div>`;
  });
  listBox.innerHTML = htmlCode;
 }
+
+
 
 // Delete
 function deleteTodo(ind) {
@@ -56,31 +57,35 @@ function deleteTodo(ind) {
  displayTodo();
 }
 
+// Edit
+
+saveTaskButton.addEventListener("click", () => {
+  let todo = localStorage.getItem("todo");
+  todoArray = JSON.parse(todo);
+  let id = saveInd.value;
+  todoArray[id] = text.value;
+  addTaskButton.style.display = "block";
+  saveTaskButton.style.display = "none";
+  text.value = "";
+  localStorage.setItem("todo", JSON.stringify(todoArray));
+  displayTodo();
+ });
 
 
-// Jasons Saturday to do list
-
-const todos = [
-  {
-    id: 1,
-    title: "Do the Washing"
-  },
-  {
-    id: 1,
-    title: "Do the Washing"
-  },
-  {
-    id: 1,
-    title: "Do the Washing"
-  },
-  {
-    id: 1,
-    title: "Do the Washing"
-  },
-]
 
 
-// write html too screen
 
 
-const todosContainer = document.querySelector()
+
+
+
+
+
+
+
+
+
+ 
+
+
+
